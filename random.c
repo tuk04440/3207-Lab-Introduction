94cbdfa569a12a24e92dab972142fadb3e02e0c5
Allen <tuk04440@temple.edu>
2023-01-20 14:43:17 -0500

    rand func and header file

---
+++ random.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "random.h"
+
+char randchar(){
+    return 'A'+ (rand() % 26);
+}
---
+++ random.h
@@ -0,0 +1,6 @@
+#ifndef random_h
+#define random_h
+
+char randchar();
+
+#endif
